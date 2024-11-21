8128d83ad79a955f5b3a31b646e784602137ca9f
Miko <meyrambek.mratbek13@gmail.com>
2024-11-21 17:49:28 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,22 @@
+number1=float(input('print a number1:'))
+operation=input('print a operation:')
+number2=float(input('print a number2:'))
+while 
+if operation=="+":
+  print(number1+number2)
+elif operation=="-":
+  print(number1-number2)
+elif operation=="*":
+  print(number1*number2)
+elif operation=="/":
+  print(number1/number2)
+elif operation==("**"):
+  print(number1**number2)
+elif operation=="%":
+  print(number1%number2)
+elif operation=="//":
+  print(number1//number2)
+elif operation=="**/":
+  print(number1**(1/number2))
+else:
+  print('error')
